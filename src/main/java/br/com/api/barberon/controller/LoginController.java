35e6ec79eb86e2b9f5d704219106dbb21be0dea9
Andreeeez <57879149+Andreeeez@users.noreply.github.com>
2020-09-13 00:47:46 -0300

    Update LoginController.java

--- src/main/java/br/com/api/barberon/controller/LoginController.java
+++ src/main/java/br/com/api/barberon/controller/LoginController.java
@@ -2,9 +2,9 @@ package br.com.api.barberon.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import br.com.api.barberon.dao.LoginDAO;
 import br.com.api.barberon.entity.Login;
 
+@CrossOrigin(origins = "localhost:3000", maxAge = 3000)
 @RestController
 @RequestMapping("/barberon/login")
 public class LoginController {
@@ -21,6 +22,7 @@ public class LoginController {
 	@GetMapping("/teste")
 	public ResponseEntity<String> teste(){
 		return new ResponseEntity<>("TESTE", HttpStatus.OK);
+		
 	}
 	
 	@PostMapping
@@ -30,7 +32,6 @@ public class LoginController {
 		String json;
 		Login l = new Login();
 		LoginDAO dao = new LoginDAO();
-		
 		l = dao.validaAcessoAoApp(login);
 		json = gson.toJson(l);
 		
